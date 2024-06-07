3e94b5f1ee277183e043788a3e9ebf89b5c844bd
Imran Baloch <imranjameel4565@gmail.com>
2024-06-07 22:59:18 +0500

    AboutMe cell.

--- WorldNoor/Controller/MarketPlace/MPProfileViewController/Cells/MPProfileAboutMeTableViewCell.swift
+++ WorldNoor/Controller/MarketPlace/MPProfileViewController/Cells/MPProfileAboutMeTableViewCell.swift
@@ -11,6 +11,14 @@ import UIKit
 class MPProfileAboutMeTableViewCell: UITableViewCell {
     static let identifier = "MPProfileAboutMeTableViewCell"
 
+    @IBOutlet weak var aboutMeHeadingLabel: UILabel!
+    @IBOutlet weak var houseIconImageView: UIImageView!
+    @IBOutlet weak var livesInLabel: UILabel!
+    @IBOutlet weak var clockIconImageView: UIImageView!
+    @IBOutlet weak var responsiveLabel: UILabel!
+    @IBOutlet weak var appLogoImageView: UIImageView!
+    @IBOutlet weak var joinedLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
